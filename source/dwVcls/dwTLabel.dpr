19e17a89263e9b82f4be865406a10051f9fe54ee
13359289126 <fenglinyushu@163.com>
2020-08-12 22:13:10 +0800

    竟然忘记了TLabel, 顺便做了一个Hello,DeWeb

---
+++ source/dwVcls/dwTLabel.dpr
@@ -0,0 +1,165 @@
+library dwTLabel;
+
+uses
+     ShareMem,
+
+     //
+     dwCtrlBase,
+
+     //
+     SynCommons,
+
+     //
+     Messages, SysUtils, Variants, Classes, Graphics,
+     Controls, Forms, Dialogs, ComCtrls, ExtCtrls,
+     StdCtrls, Windows;
+
+function _GetFont(AFont:TFont):string;
+begin
+     Result    := 'color:'+dwColor(AFont.color)+';'
+               +'font-family:'''+AFont.name+''';'
+               +'font-size:'+IntToStr(AFont.size)+'pt;';
+
+     //粗体
+     if fsBold in AFont.Style then begin
+          Result    := Result+'font-weight:bold;';
+     end else begin
+          Result    := Result+'font-weight:normal;';
+     end;
+
+     //斜体
+     if fsItalic in AFont.Style then begin
+          Result    := Result+'font-style:italic;';
+     end else begin
+          Result    := Result+'font-style:normal;';
+     end;
+
+     //下划线
+     if fsUnderline in AFont.Style then begin
+          Result    := Result+'text-decoration:underline;';
+          //删除线
+          if fsStrikeout in AFont.Style then begin
+               Result    := Result+'text-decoration:line-through;';
+          end;
+     end else begin
+          //删除线
+          if fsStrikeout in AFont.Style then begin
+               Result    := Result+'text-decoration:line-through;';
+          end else begin
+               Result    := Result+'text-decoration:none;';
+          end;
+     end;
+end;
+
+function _GetAlignment(ACtrl:TControl):string;
+begin
+     Result    := '';
+     case TLabel(ACtrl).Alignment of
+          taRightJustify : begin
+               Result    := 'text-align:right;';
+          end;
+          taCenter : begin
+               Result    := 'text-align:center;';
+          end;
+     end;
+end;
+
+
+//当前控件需要引入的第三方JS/CSS
+function dwGetExtra(ACtrl:TComponent):string;stdCall;
+begin
+     Result    := '[]';
+end;
+
+//根据JSON对象AData执行当前控件的事件, 并返回结果字符串
+function dwGetEvent(ACtrl:TComponent;AData:String):string;StdCall;
+begin
+     //
+     if Assigned(TLabel(ACtrl).OnClick) then begin
+          TLabel(ACtrl).OnClick(TLabel(ACtrl));
+     end;
+end;
+
+
+//取得HTML头部消息
+function dwGetHead(ACtrl:TComponent):string;StdCall;
+var
+     sCode     : string;
+     joHint    : Variant;
+     joRes     : Variant;
+begin
+     //生成返回值数组
+     joRes    := _Json('[]');
+
+     //取得HINT对象JSON
+     joHint    := dwGetHintJson(TControl(ACtrl));
+
+     with TLabel(ACtrl) do begin
+          sCode     := '<div '
+                    +' v-html="'+Name+'__cap"'
+                    +dwVisible(TControl(ACtrl))
+                    +dwDisable(TControl(ACtrl))
+                    +dwLTWH(TControl(ACtrl))
+                    +_GetFont(Font)
+                    //style
+                    +_GetAlignment(TControl(ACtrl))
+                    +dwIIF(Layout=tlCenter,'line-height:'+IntToStr(Height)+'px;','')
+                    +'"'
+                    //style 封闭
+
+                    +dwIIF(Assigned(OnClick),Format(_DWEVENT,['click',Name,'0','onclick','']),'')
+                    +'>{{'+Name+'__cap}}';
+          //添加到返回值数据
+          joRes.Add(sCode);
+     end;
+     //
+     Result    := (joRes);
+end;
+
+//取得HTML尾部消息
+function dwGetTail(ACtrl:TComponent):string;StdCall;
+var
+     joRes     : Variant;
+begin
+     //生成返回值数组
+     joRes    := _Json('[]');
+     //生成返回值数组
+     joRes.Add('</div>');
+     //
+     Result    := (joRes);
+end;
+
+//取得Data消息, ASeparator为分隔符, 一般为:或=
+function dwGetData(ACtrl:TComponent;ASeparator:String):string;StdCall;
+var
+     joRes     : Variant;
+begin
+     //生成返回值数组
+     joRes    := _Json('[]');
+     //
+     with TLabel(ACtrl) do begin
+          joRes.Add(Name+'__lef'+ASeparator+'"'+IntToStr(Left)+'px"');
+          joRes.Add(Name+'__top'+ASeparator+'"'+IntToStr(Top)+'px"');
+          joRes.Add(Name+'__wid'+ASeparator+'"'+IntToStr(Width)+'px"');
+          joRes.Add(Name+'__hei'+ASeparator+'"'+IntToStr(Height)+'px"');
+          //
+          joRes.Add(Name+'__vis'+ASeparator+''+dwIIF(Visible,'true','false'));
+          joRes.Add(Name+'__dis'+ASeparator+''+dwIIF(Enabled,'false','true'));
+          //
+          joRes.Add(Name+'__cap'+ASeparator+'"'+dwProcessCaption(Caption)+'"');
+     end;
+     //
+     Result    := (joRes);
+end;
+
+
+exports
+     dwGetExtra,
+     dwGetEvent,
+     dwGetHead,
+     dwGetTail,
+     dwGetData;
+     
+begin
+end.
+ 
